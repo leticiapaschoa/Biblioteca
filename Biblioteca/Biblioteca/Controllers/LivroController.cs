bb5a733d188390031ea551175688f0289ec5d905
Leticia <leticia.paschoa.silva@gmail.com>
2018-12-03 20:42:55 -0200

    Telas Livro

--- Biblioteca/Biblioteca/Controllers/LivroController.cs
+++ Biblioteca/Biblioteca/Controllers/LivroController.cs
@@ -12,7 +12,7 @@ namespace Biblioteca.Controllers
     public class LivroController : Controller
     {
         private Context db = new Context();
-     
+
         public ActionResult Index()
         {
             List<Livro> livro = db.Livro.ToList();
@@ -45,5 +45,66 @@ namespace Biblioteca.Controllers
 
             return View(livro);
         }
+
+        public ActionResult EditarLivro(int id)
+        {
+            Livro livro = db.Livro.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        [HttpPost]
+        public ActionResult EditarLivro([Bind(Include = "LivroID,Titulo,Editora,Autor")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(livro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(livro);
+        }
+
+        public ActionResult ExcluirLivro(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livro.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        [HttpPost, ActionName("ExcluirLivro")]
+        public ActionResult ExcluirConfirma(int id)
+        {
+            Livro livro = db.Livro.Find(id);
+            db.Livro.Remove(livro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DetalhesLivro(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livro.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
     }
+
 }
